d94fc2c5ce523e690a39164ef6def5b5482b93e9
Cris W Zhu <cris.w.zhu@pwc.com>
2018-01-02 17:31:45 +0800

    add bootstrap

--- src/main/java/com/cris/website/controller/TestController.java
+++ src/main/java/com/cris/website/controller/TestController.java
@@ -21,9 +21,6 @@ public class TestController {
 	
 	@RequestMapping(value="test")
     public ModelAndView indexPage(HttpServletRequest request,HttpServletResponse response) throws JsonConverterException{
-		String string="{\"HeWeather5\":[{\"basic\":{\"city\":\"上海\",\"cnty\":\"中国\",\"id\":\"CN101020100\",\"lat\":\"31.23170662\",\"lon\":\"121.47264099\",\"update\":{\"loc\":\"2017-09-05 14:46\",\"utc\":\"2017-09-05 06:46\"}},\"now\":{\"cond\":{\"code\":\"101\",\"txt\":\"多云\"},\"fl\":\"37\",\"hum\":\"60\",\"pcpn\":\"0\",\"pres\":\"1006\",\"tmp\":\"33\",\"vis\":\"6\",\"wind\":{\"deg\":\"96\",\"dir\":\"东风\",\"sc\":\"微风\",\"spd\":\"4\"}},\"status\":\"ok\"}]}";
-		HeWeather heWeather=JacksonHelper.fromJSON(string, HeWeather.class);
-        return new ModelAndView("indexPage");
+        return new ModelAndView("testPage");
     }
 }
-
