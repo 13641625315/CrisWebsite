d9260380a796f6d2e5e01438c48c8521faa1f22c
Cris W Zhu <cris.w.zhu@pwc.com>
2018-01-23 17:24:01 +0800

    test fix

--- src/main/java/com/cris/website/controller/TestController.java
+++ src/main/java/com/cris/website/controller/TestController.java
@@ -3,6 +3,8 @@ package com.cris.website.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +15,9 @@ public class TestController {
 	
 	@RequestMapping(value="test")
     public ModelAndView indexPage(HttpServletRequest request,HttpServletResponse response,Model model){
+		final Log log = LogFactory.getLog(getClass());
+		log.debug("TEST PAGE!");
+		log.error("TEST PAGE!");
 		model.addAttribute("pageTitle", "测试页");
         return new ModelAndView("testPage");
     }
